1e8dc7232f92591681085633b4e923d64851531c
Adrian Pop <pop.adrian61@gmail.com>
2018-08-21 15:11:32 +0300

    initial commit; create the applet and add the menu items;

---
+++ applet.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+from playback.internet_radio import internetRadio
+
+import gi
+
+gi.require_version('MatePanelApplet', '4.0')
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk
+from gi.repository import MatePanelApplet
+
+class DialogWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="About")
+
+        self.set_border_width(6)
+        label = Gtk.Label('This is a radio streamer')
+        button = Gtk.Button("Close")
+        button.connect("clicked", lambda a: self.destroy())
+
+        self.add(button)
+
+
+def on_button_clicked(widget):
+
+    if internetRadio.is_playing():
+        internetRadio.stop()
+    else:
+        internetRadio.play_station(0)
+
+
+def display_preferences_dialog(action, applet):
+    print("Preferences")
+
+
+def display_help_dialog(action, applet):
+    print("Help")
+
+
+def display_about_dialog(action, applet):
+    DialogWindow().show_all()
+
+
+player_menu_verbs = [
+    ("PlayerPreferences", "document-properties", "_Preferences",
+     None, None, display_preferences_dialog),
+    ("PlayerHelp", "help-browser", "_Help",
+    None, None, display_help_dialog),
+    ("PlayerAbout", "help-about", "_About",
+     None, None, display_about_dialog),
+]
+
+
+def applet_fill(applet):
+    action_group = Gtk.ActionGroup("Applet actions")
+    action_group.add_actions(player_menu_verbs, applet)
+    applet.setup_menu_from_file("menu.xml", action_group)
+
+    settings_path = applet.get_preferences_path()
+
+    button = Gtk.Button("Play")
+    button.connect("clicked", on_button_clicked)
+    applet.add(button)
+
+
+    applet.show_all()
+
+
+def applet_factory(applet, iid, data):
+    if iid != "TestApplet":
+       return False
+    applet_fill(applet)
+    return True
+
+
+
+MatePanelApplet.Applet.factory_main("TestAppletFactory", True,
+                                    MatePanelApplet.Applet.__gtype__,
+                                    applet_factory, None)
---
+++ playback/internet_radio.py
@@ -0,0 +1,194 @@
+from collections import namedtuple
+import threading
+import subprocess
+import requests
+import re
+
+from . playback import Output
+from . sample import Sample
+
+
+class IceCastClient:
+    """
+    A simple client for IceCast audio streams.
+    The stream method yields blocks of encoded audio data from the stream.
+    If the stream has Icy Meta Data, the stream_title attribute will be updated
+    with the actual title taken from the meta data.
+    """
+    def __init__(self, url, block_size=16384):
+        self.url = url
+        self.stream_format = "???"
+        self.stream_title = "???"
+        self.station_genre = "???"
+        self.station_name = "???"
+        self.block_size = block_size
+        self._stop_stream = False
+
+    def stop_streaming(self):
+        self._stop_stream = True
+
+    def stream(self):
+        with requests.get(self. url, stream=True, headers={"icy-metadata": "1"}) as result:
+            self.station_genre = result.headers["icy-genre"]
+            self.station_name = result.headers["icy-name"]
+            self.stream_format = result.headers["Content-Type"]
+            if "icy-metaint" in result.headers:
+                meta_interval = int(result.headers["icy-metaint"])
+            else:
+                meta_interval = 0
+            if meta_interval:
+                audiodata = b""
+                for chunk in result.iter_content(self.block_size):
+                    if self._stop_stream:
+                        return
+                    audiodata += chunk
+                    if len(audiodata) < meta_interval + 1:
+                        continue
+                    meta_size = 16 * audiodata[meta_interval]
+                    if len(audiodata) < meta_interval + 1 + meta_size:
+                        continue
+                    metadata = str(audiodata[meta_interval + 1: meta_interval + 1 + meta_size].strip(b"\0"), "utf-8")
+                    if metadata:
+                        self.stream_title = re.search("StreamTitle='(.*?)'", metadata).group(1)
+                    yield audiodata[:meta_interval]
+                    audiodata = audiodata[meta_interval + 1 + meta_size:]
+                    if self._stop_stream:
+                        return
+            else:
+                for chunk in result.iter_content(self.block_size):
+                    if self._stop_stream:
+                        break
+                    yield chunk
+
+
+class AudioDecoder:
+    """
+    Reads streaming audio from an IceCast stream,
+    decodes it using ffmpeg, and plays it on the output sound device.
+
+    We need two threads:
+     1) main thread that spawns ffmpeg, reads radio stream data, and writes that to ffmpeg
+     2) background thread that reads decoded audio data from ffmpeg and plays it
+    """
+    def __init__(self, icecast_client, song_title_callback=None):
+        self.client = icecast_client
+        self.stream_title = "???"
+        self.song_title_callback = song_title_callback
+        self.ffmpeg_process = None
+
+    def stop_playback(self):
+        if self.ffmpeg_process:
+            self.ffmpeg_process.stdin.close()
+            self.ffmpeg_process.stdout.close()
+            self.ffmpeg_process.kill()
+            self.ffmpeg_process = None
+
+    def _audio_playback(self, ffmpeg_stream):
+        # thread 3: audio playback
+
+        def played(sample):
+            if self.client.stream_title != self.stream_title:
+                self.stream_title = self.client.stream_title
+                if self.song_title_callback:
+                    self.song_title_callback(self.stream_title)
+                else:
+                    print("\n\nNew Song:", self.stream_title, "\n")
+
+        with Output(mixing="sequential", frames_per_chunk=44100//4) as output:
+            output.register_notify_played(played)
+            while True:
+                try:
+                    audio = ffmpeg_stream.read(44100 * 2 * 2 // 10)
+                    if not audio:
+                        break
+                except (IOError, ValueError):
+                    break
+                else:
+                    sample = Sample.from_raw_frames(audio, 2, 44100, 2)
+                    output.play_sample(sample)
+
+    def stream_radio(self):
+        stream = self.client.stream()
+        first_chunk = next(stream)
+        format = ""
+        if self.client.stream_format == "audio/mpeg":
+            format = "mp3"
+        elif self.client.stream_format.startswith("audio/aac"):
+            format = "aac"
+        if not self.song_title_callback:
+            print("\nStreaming Radio Station: ", self.client.station_name)
+        cmd = ["ffmpeg", "-v", "fatal", "-nostdin", "-i", "-"]
+        if format:
+            cmd.extend(["-f", format])
+        # cmd.extend(["-af", "aresample=resampler=soxr"])     # enable this if your ffmpeg has sox hq resample
+        cmd.extend(["-ar", "44100", "-ac", "2", "-acodec", "pcm_s16le", "-f", "s16le", "-"])
+        self.ffmpeg_process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.ffmpeg_process.stdin.write(first_chunk)
+        audio_playback_thread = threading.Thread(target=self._audio_playback, args=[self.ffmpeg_process.stdout], daemon=True)
+        audio_playback_thread.start()
+
+        try:
+            for chunk in stream:
+                if self.ffmpeg_process:
+                    self.ffmpeg_process.stdin.write(chunk)
+                else:
+                    break
+        except BrokenPipeError:
+            pass
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.stop_playback()
+            audio_playback_thread.join()
+            if not self.song_title_callback:
+                print("\n")
+
+
+class Internetradio:
+    StationDef = namedtuple("StationDef", ["station_name", "stream_name", "icon_url", "stream_url"])
+    stations = [
+        StationDef("Soma FM", "Groove Salad",
+                   "http://somafm.com/img/groovesalad120.png",
+                   "http://ice3.somafm.com/groovesalad-64-aac"),
+        StationDef("Soma FM", "Secret Agent",
+                   "http://somafm.com/img/secretagent120.jpg",
+                   "http://ice3.somafm.com/secretagent-64-aac"),
+        StationDef("University of Calgary", "CJSW-FM",
+                   "https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/CJSW-FM.svg/220px-CJSW-FM.svg.png",
+                   "http://stream.cjsw.com:80/cjsw.ogg"),
+        StationDef("Playtrance.com", "Trance",
+                   "http://facilmediamundial.com/wp-content/uploads/2017/09/trance-200.png",
+                   "http://live.playtrance.com:8000/playtrance-livetech.aac")
+    ]
+
+    def __init__(self):
+        self.song_title = "..."
+        self.play_thread = None
+
+    def play_station(self, index):
+        station = self.stations[index]
+        if self.is_playing():
+            self.stop()
+        self.stream_name_label = "{} | {}".format(station.station_name, station.stream_name)
+        self.icyclient = IceCastClient(station.stream_url, 8192)
+        self.decoder = AudioDecoder(self.icyclient, self.set_song_title)
+        self.set_song_title("...")
+        self.play_thread = threading.Thread(target=self.decoder.stream_radio, daemon=True)
+        self.play_thread.start()
+
+    def set_song_title(self, title):
+        self.song_title = title
+
+    def is_playing(self):
+        return self.play_thread != None
+
+    def stop(self):
+        self.song_title = "(stoping...)"
+        self.icyclient.stop_streaming()
+        #self.decoder.stop_playback()   # this doesn't work properly on Windows, it hangs. Therefore we close the http stream.
+        self.decoder = None
+        self.play_thread.join()
+        self.play_thread = None
+
+
+internetRadio = Internetradio()
